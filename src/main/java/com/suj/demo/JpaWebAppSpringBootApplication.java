0677d8bbc0cbc2c71fab3b0d3b327bd318c59740
Sujeet09K <teejus09@gmail.com>
2020-09-07 00:32:46 -0400

    First commit for JPA web App spring boot project

---
+++ src/main/java/com/suj/demo/JpaWebAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.suj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWebAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWebAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suj/demo/JpaWebAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWebAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
